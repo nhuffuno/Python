1c124b59dd0ca9732ead8b16fee3e2fb426d91c2
nhuffuno <nhuff@unomaha.edu>
2024-11-19 16:53:06 -0600

    Update television.py

--- Python/television.py
+++ Python/television.py
@@ -11,16 +11,13 @@ class Television:
         self.__channel = Television.MIN_CHANNEL
 
     def power(self) -> None:
-        """Toggle the power status of the TV."""
         self.__status = not self.__status
 
     def mute(self) -> None:
-        """Mute or unmute the TV."""
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self) -> None:
-        """Increase the channel by 1 or cycle to the minimum if at the maximum."""
         if self.__status:
             if self.__channel < Television.MAX_CHANNEL:
                 self.__channel += 1
@@ -28,7 +25,6 @@ class Television:
                 self.__channel = Television.MIN_CHANNEL
 
     def channel_down(self) -> None:
-        """Method to decrease the channel."""
         if self.__status:
             if self.__channel > Television.MIN_CHANNEL:
                 self.__channel -= 1
@@ -36,21 +32,18 @@ class Television:
                 self.__channel = Television.MAX_CHANNEL
 
     def volume_up(self) -> None:
-        """Increase the volume by 1 or remain at the maximum volume."""
         if self.__status:
             self.__muted = False
             if self.__volume < Television.MAX_VOLUME:
                 self.__volume += 1
 
     def volume_down(self) -> None:
-        """Decrease the volume by 1 or remain at the minimum volume."""
         if self.__status:
             self.__muted = False
             if self.__volume > Television.MIN_VOLUME:
                 self.__volume -= 1
 
     def __str__(self) -> str:
-        """Method to show the TV's current status."""
         if self.__muted:
             return f"Power = {self.__status}, Channel = {self.__channel}, Volume = MUTED"
         else:
